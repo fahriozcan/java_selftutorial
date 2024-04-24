39ee90ed5689544d0852a99779fc0c7c80b23104
fahriozcan <fahriozcnn@gmail.com>
2024-04-24 12:30:29 +0200

    requirement added

---
+++ src/test/java/self_tutorial/TaxCalculation.java
@@ -0,0 +1,20 @@
+package self_tutorial;
+
+import org.junit.jupiter.api.Test;
+
+public class TaxCalculation {
+
+    @Test
+    public void netIncomeCalculator() {
+
+        /*
+        Requirement: Worker gets gross income and there is certain tax rate according to
+        level of the gross income. Under 12000(included) monthly there is no tax payment, between 12000-33000(included) tax rate is 20%
+        between 33000-60000(included) tax rate is 28%, between 60000-10000(included) tax rate is 35%, for over income tax rate will be 41%
+        Program should display the net income for the user
+         */
+
+
+
+    }
+}
