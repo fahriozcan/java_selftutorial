1deda7271a31456810a6817a8b16fe45b80eeee1
fahriozcan <fahriozcnn@gmail.com>
2024-05-04 21:17:26 +0200

    remove duplication is completed

--- src/test/java/self_tutorial/RemoveDublicatesFromWord.java
+++ src/test/java/self_tutorial/RemoveDublicatesFromWord.java
@@ -2,12 +2,27 @@ package self_tutorial;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class RemoveDublicatesFromWord {
 
-    @Test
-    public void test() {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Write your word to remove duplicated characters in the word");
 
+        String word = scan.next().toLowerCase();
+        int length = word.length();
+        String result = "";
 
+        for (int i = 0; i < length; i++) {
+            char ch = word.charAt(i);
+
+            if(!result.contains(ch+"")){
+                result += ch;
+            }
+        }
+        System.out.println("After removing the duplicated character = "+result);
     }
+
 }
