7ffaa6e34d043ae272a35d66048ce07c50d31090
fahriozcan <fahriozcnn@gmail.com>
2024-05-07 21:30:28 +0200

    requirement is added

---
+++ src/test/java/self_tutorial/GradeResult.java
@@ -0,0 +1,15 @@
+package self_tutorial;
+
+public class GradeResult {
+
+    /*
+    Based on the given score between 0-100, user will know the result
+    if the result is less than 60, result is failed
+    if the score is more than or equal to 60, result is passed
+    score should be between 0-100, otherwise result is invalid
+     */
+
+
+
+
+}
