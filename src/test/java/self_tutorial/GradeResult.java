b6374b7cebc4de8abfd3056a7af927e82421f643
fahriozcan <fahriozcnn@gmail.com>
2024-05-07 21:41:00 +0200

    ternary solution

--- src/test/java/self_tutorial/GradeResult.java
+++ src/test/java/self_tutorial/GradeResult.java
@@ -1,5 +1,7 @@
 package self_tutorial;
 
+import org.junit.jupiter.api.Test;
+
 public class GradeResult {
 
     /*
@@ -7,9 +9,17 @@ public class GradeResult {
     if the result is less than 60, result is failed
     if the score is more than or equal to 60, result is passed
     score should be between 0-100, otherwise result is invalid
+    solve the problem with if statement, switch statement and ternary statement
      */
 
+    @Test
+    public void test() {
 
+        int score = 101;
 
+        String result = (score >= 0 && score <= 100) ? (score < 60) ? "Failed" : "Passed" : "Invalid Score";
 
+        System.out.println("result = " + result);
+
+    }
 }
